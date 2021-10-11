b00350c5fae625dd8b7999f8b3b881444e035594
Jinwei Hu <jinwei.hu@edu.sait.ca>
2021-10-10 20:40:39 -0600

    Creating and Removing

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -21,26 +21,37 @@ public class NoteServlet extends HttpServlet {
             throws ServletException, IOException {
     
         String URL;
-        String href;
         String path = getServletContext().getRealPath("/WEB-INF/note.txt");
         
         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
         notes = NoteSerializer.readNoteTextFile(br);
         
-        href = request.getParameter("edit");
+        String editIndex = request.getParameter("edit");
+        String createParam = request.getParameter("create");
+        String deleteParam = request.getParameter("delete");
         
-        if (href == null){
-            request.setAttribute("CONTENTS", notes);
-            URL = "/WEB-INF/viewnote.jsp";   
-            getServletContext().getRequestDispatcher(URL).forward(request, response);            
-        }
-        else
-        {
-            int index = Integer.valueOf(href);
+        if (editIndex != null){
+            // edit
+            int index = Integer.valueOf(editIndex);
             request.setAttribute("CONTENTS", notes.get(index));
             request.setAttribute("NOTE_ID", index);
             URL = "/WEB-INF/editnote.jsp";  
+            getServletContext().getRequestDispatcher(URL).forward(request, response);         
+        } else if (createParam != null) {
+            // create
+            URL = "/WEB-INF/createnote.jsp";
             getServletContext().getRequestDispatcher(URL).forward(request, response);
+        } else if (deleteParam != null) {
+            // delete
+            int index = Integer.valueOf(deleteParam);
+            notes.remove(index);
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+            NoteSerializer.SaveNotesToTextFile(pw, notes);
+            response.sendRedirect("/note");  
+        } else {
+            request.setAttribute("CONTENTS", notes);
+            URL = "/WEB-INF/viewnote.jsp";   
+            getServletContext().getRequestDispatcher(URL).forward(request, response);   
         }
     }
 
@@ -53,18 +64,24 @@ public class NoteServlet extends HttpServlet {
         String path;
         String title = request.getParameter("EDIT_TITLE");
         String contents = request.getParameter("EDIT_CONTENTS");
-        int index = Integer.valueOf(request.getParameter("save"));
-        System.out.println(index);
         URL = "/WEB-INF/viewnote.jsp";
-        Note fileContents;
-        fileContents = new Note(title, contents);
-        notes.set(index, fileContents);
+
+        
+        String save = request.getParameter("save");
+        String create = request.getParameter("create");
+        
+        if (save != null) {
+            int saveIndex = Integer.valueOf(save);
+            Note fileContents = new Note(title, contents);
+            notes.set(saveIndex, fileContents);
+        } else if (create != null) {
+            notes.add(new Note(title, contents));
+        }
 
         path = getServletContext().getRealPath("/WEB-INF/note.txt");
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
         NoteSerializer.SaveNotesToTextFile(pw, notes);
 
-        request.setAttribute("CONTENTS", fileContents);
         response.sendRedirect("/note");  
     }
 }
---
+++ web/WEB-INF/createnote.jsp
@@ -0,0 +1,29 @@
+<%-- 
+    Document   : createnote
+    Created on : Oct 10, 2021, 8:34:14 PM
+    Author     : jinwe
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>View Note - Simple Note Keeper</title>
+    </head>
+    <body>
+           <h1>Simple Note Keeper</h1>
+
+        <form action="note?create" method="POST">
+            <h2>Create Note</h2>
+            
+            <h3>Title:</h3>
+            <input type="text" name="EDIT_TITLE" value="">
+            <h3>Contents:</h3>
+            <textarea type="text" name="EDIT_CONTENTS" style="height:100px; width:300px;"></textarea>
+           
+            <input type="submit" value="Save">   
+    </body>
+</html>
